5a6d422f88bf49b21f9e28da1b83be04001bdc70
Marcelo <Marcelo@DESKTOP-U8LS67D>
2026-09-15 21:32:10 -0300

    Pruebas contador

--- Js/visitas.js
+++ Js/visitas.js
@@ -1,3 +1,28 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+    // const contador = document.getElementById("contador-visitas");
+
+    // const url = "https://abacus.jasoncameron.dev/hit/hefesto-sii-docs/visitas";
+
+    // fetch(url)
+        // .then(response => {
+            // if (!response.ok) {
+                // throw new Error(`HTTP ${response.status}`);
+            // }
+
+            // return response.json();
+        // })
+        // .then(data => {
+            // contador.textContent = `Visitas: ${data.value}`;
+        // })
+        // .catch(error => {
+            // console.error("Error al obtener el contador:", error);
+            // contador.textContent = "Visitas: no disponible";
+        // });
+
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const contador = document.getElementById("contador-visitas");
@@ -5,12 +30,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const url = "https://abacus.jasoncameron.dev/hit/hefesto-sii-docs/visitas";
 
     fetch(url)
-        .then(response => {
+        .then(async response => {
+
+            const texto = await response.text();
+
+            console.log("HTTP:", response.status);
+            console.log("Respuesta Abacus:", texto);
+
             if (!response.ok) {
                 throw new Error(`HTTP ${response.status}`);
             }
 
-            return response.json();
+            return JSON.parse(texto);
         })
         .then(data => {
             contador.textContent = `Visitas: ${data.value}`;
