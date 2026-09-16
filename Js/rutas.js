39a36a5c12e2afab8514a19948b87826a9b8f851
Marcelo <Marcelo@DESKTOP-U8LS67D>
2026-09-16 11:40:07 -0300

    links

--- Js/rutas.js
+++ Js/rutas.js
@@ -1,8 +1,9 @@
-const BASE_URL = window.location.hostname.includes('github.io')
-    ? '/SiiNugetDocumentacion/'
-    : '/';
-
-document.querySelector("base").href = base;	
-	
-// Genera una ruta desde la raíz del sitio 
-function ruta(path) { return BASE_URL + path.replace(/^\/+/, ""); }
+function raizSitio() {
+    if (window.location.hostname.includes("github.io")) {
+        return "/SiiNugetDocumentacion/";
+    }
+    return "/";
+}
+function ir(ruta) {
+    window.location.href = raizSitio() + ruta;
+}
